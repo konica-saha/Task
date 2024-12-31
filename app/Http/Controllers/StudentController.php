c47954f9398233e5686155bdedf22a6821a0fbf3
konica saha <sahakonica2435@gmail.com>
2024-12-31 19:22:28 +0600

    First commit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\{Student, Department, Course, Session};
 use Illuminate\Http\Request;
 
@@ -8,25 +9,26 @@ class StudentController extends Controller
 {
     public function index(Request $request)
     {
-        $students = Student::query();
+        $query = Student::query();
 
         if ($request->filled('department_id')) {
-            $students->where('department_id', $request->department_id);
+            $query->where('department_id', $request->department_id);
         }
 
         if ($request->filled('course_id')) {
-            $students->where('course_id', $request->course_id);
+            $query->where('course_id', $request->course_id);
         }
 
         if ($request->filled('session_id')) {
-            $students->where('session_id', $request->session_id);
+            $query->where('session_id', $request->session_id);
         }
 
-        return view('students.index', [
-            'students' => $students->get(),
-            'departments' => Department::all(),
-            'courses' => Course::all(),
-            'sessions' => Session::all(),
-        ]);
+      
+        $students = $query->get();
+        $departments = Department::all();
+        $courses = Course::all();
+        $sessions = Session::all();
+
+        return view('students.index', compact('students', 'departments', 'courses', 'sessions'));
     }
 }
